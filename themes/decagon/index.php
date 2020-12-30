72743a564fef9e437f71fd8f7bb5c498d1a009f4
mystevotgit <oyeolatunji@gmail.com>
2020-12-31 00:27:02 +0100

    add engineers section as a template part

--- themes/decagon/index.php
+++ themes/decagon/index.php
@@ -348,30 +348,7 @@ $ac5 = get_field('achievement5');
           </div>
         </div>
       </div>
-      <div class="our-skills container">
-        <h4>Skilled in technologies you need</h4>
-        <h6>
-          Access a pool of skilled engineers to match your product requirements
-        </h6>
-        <ul>
-          <li>Back-end engineer</li>
-          <li>Front-end engineer</li>
-          <li>Full-Stack engineer</li>
-          <li>Mobile Engineer</li>
-          <li>QA Engineer</li>
-          <li>React Developer</li>
-          <li>Android Developer</li>
-          <li>IOS Developer</li>
-          <li>React Native Developer</li>
-          <li>Python Developer</li>
-          <li>Node.JS Developer</li>
-          <li>Javascript Developer</li>
-          <li>Java Developer</li>
-          <li>C# Developer</li>
-          <li>Devops Engineer</li>
-          <li>UI/UX Developer</li>
-        </ul>
-      </div>
+      <?php get_template_part('template_parts/engineers', 'section'); ?>
     </section>
 
     <section class="section-media">
---
+++ themes/decagon/template_parts/engineers-section.php
@@ -0,0 +1,24 @@
+<div class="our-skills container">
+        <h4>Skilled in technologies you need</h4>
+        <h6>
+          Access a pool of skilled engineers to match your product requirements
+        </h6>
+        <ul>
+          <li>Back-end engineer</li>
+          <li>Front-end engineer</li>
+          <li>Full-Stack engineer</li>
+          <li>Mobile Engineer</li>
+          <li>QA Engineer</li>
+          <li>React Developer</li>
+          <li>Android Developer</li>
+          <li>IOS Developer</li>
+          <li>React Native Developer</li>
+          <li>Python Developer</li>
+          <li>Node.JS Developer</li>
+          <li>Javascript Developer</li>
+          <li>Java Developer</li>
+          <li>C# Developer</li>
+          <li>Devops Engineer</li>
+          <li>UI/UX Developer</li>
+        </ul>
+      </div>
