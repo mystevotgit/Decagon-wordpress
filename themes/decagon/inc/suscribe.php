f4dbbe04b8ae310601cb161f23236fd5d6ad83a1
johnbosco <nwekejohnbosco@gmail.com>
2021-02-11 19:41:58 +0100

    add subscribe form from inc folder

---
+++ themes/decagon/inc/suscribe.php
@@ -0,0 +1,13 @@
+            <div class="single-page-body2">
+                <div class="subscribe-title">
+                    <h3>Subscribe</h3>
+                    <h4>Get latest insights on engineering and business</h4>
+                </div>
+                <form action="">
+                    <label for="#name">Your name</label>
+                    <input id="name" type="text" placeholder="Enter first name">
+                    <label for="#email">Your email</label>
+                    <input id="email" type="text" placeholder="name@example.com">
+                    <input class="submitbtn" type="submit">
+                </form>
+            </div>
